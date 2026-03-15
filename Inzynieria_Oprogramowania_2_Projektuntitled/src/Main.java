cd7cc349eb98c3489a746be528b85ac3b211f4ee
mk05pl <michal.kowalski099884@wp.pl>
2026-03-15 13:18:29 +0100

    Test commit and push

--- Inzynieria_Oprogramowania_2_Projektuntitled/src/Main.java
+++ Inzynieria_Oprogramowania_2_Projektuntitled/src/Main.java
@@ -3,7 +3,7 @@
 void main() {
     //TIP Naciśnij <shortcut actionId="ShowIntentionActions"/>, gdy kursor znajduje się na wyróżnionym tekście
     // aby zobaczyć, jak IntelliJ IDEA sugeruje to naprawić.
-    IO.println(String.format("Hello and welcome!"));
+    IO.println(String.format("Hello and welcome siema!"));
 
     for (int i = 1; i <= 5; i++) {
         //TIP Naciśnij <shortcut actionId="Debug"/>, aby rozpocząć debugowanie kodu. Ustawiliśmy jeden punkt przerwania <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
