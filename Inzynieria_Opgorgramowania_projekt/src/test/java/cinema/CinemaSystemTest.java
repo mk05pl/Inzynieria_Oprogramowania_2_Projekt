06b4746e40ebf85ef7e49dd46a465a42fa15c09a
Stin001 <isiek2004@gmail.com>
2026-04-25 01:05:16 +0200

    Add files via upload

---
+++ Inzynieria_Opgorgramowania_projekt/src/test/java/cinema/CinemaSystemTest.java
@@ -0,0 +1,25 @@
+package cinema;
+
+import cinema.model.Customer;
+import cinema.model.Employee;
+import cinema.model.Film;
+import cinema.model.Hall;
+import cinema.model.Manager;
+import cinema.model.Screening;
+import cinema.model.Seat;
+import cinema.model.Ticket;
+import cinema.model.User;
+import cinema.service.CinemaSystem;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Testy jednostkowe dla klasy CinemaSystem i powiązanych modeli.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CinemaSystemTest {
+
+}
